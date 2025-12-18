c4b92808af339aa74f4c854f4cd2685c6f98490a
Mimansha Sharma <mimanshasharmaofficial@gmail.com>
2025-12-18 23:56:01 +0530

    Add files via upload

---
+++ Javascript/part1/changes.js
@@ -0,0 +1,6 @@
+let gameName = "Badminton";
+gameName = "Football";
+console.log(gameName);
+ 
+const username = "Mimansha";
+console.log(username);
---
+++ Javascript/part1/datatype.js
@@ -0,0 +1,24 @@
+console.log("hello");
+console.log("hello world");
+/*
+
+String
+Number
+Boolean
+Bigint
+Undefined
+null 
+0(temperature)
+Object
+Symbol
+
+*/
+//var score = 1912;
+let score = 1912;
+let name = "Mimansha";
+let isLoggedin = false;
+// object
+let foodTypes = ["South Indian"];
+let user = { firstname: "Mimansha", lastname: "Sharma", age: 20, }
+let getScore = score;
+console.log(getScore);
