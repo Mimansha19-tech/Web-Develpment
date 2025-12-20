9ec5c221757e77bd8870cb2b7b1766c2eb6193ac
Mimansha Sharma <mimanshasharmaofficial@gmail.com>
2025-12-20 23:48:42 +0530

    Add files via upload

---
+++ Javascript/part1/operations.js
@@ -0,0 +1,29 @@
+let score = 102;
+let bonus = 50;
+let totalScore = score + bonus;
+
+
+
+let addition = 4 + 5;
+let sutract = 9 - 3;
+let multi = 3 * 5;
+let div = 8 / 2;
+let reminder = 9 % 2;
+
+
+let expo = 2 ** 3;  // 2^3 = 2*2*2 = 8
+
+
+let myscore = 110;
+myscore++;
+let credits = 56;
+credits--;
+//Comparison Operations
+let num1 = 3;
+let num2 = 5;
+let num3 = 9;
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
+
