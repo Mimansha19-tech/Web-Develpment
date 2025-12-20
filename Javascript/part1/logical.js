435be26bbcd5511875c474a0e2ebc3fa17f6d45a
Mimansha Sharma <mimanshasharmaofficial@gmail.com>
2025-12-20 23:49:53 +0530

    Add files via upload

---
+++ Javascript/part1/logical.js
@@ -0,0 +1,9 @@
+// && -> and
+// || -> or
+// ! -> not (reverse)
+let isLoggedin = false;
+let ispaid = true;
+console.log(isLoggedin && ispaid);
+let isEmailuser = true;
+let isGoogleuser = false;
+console.log(isEmailuser || isGoogleuser);
